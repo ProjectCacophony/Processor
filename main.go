ec7152b36215cfbba17090f070b10aa2797b6056
Sebastian Winkler <sebastian.winkler@slmn.de>
2018-04-22 19:19:34 +0200

    uses new dhelpers components

--- main.go
+++ main.go
@@ -1,18 +1,14 @@
 package main
 
 import (
-	"flag"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sqs"
-	"github.com/go-redis/redis"
 	"github.com/json-iterator/go"
-	"github.com/sirupsen/logrus"
 	"gitlab.com/project-d-collab/SqsProcessor/modules"
 	"gitlab.com/project-d-collab/dhelpers"
 	dhelpersCache "gitlab.com/project-d-collab/dhelpers/cache"
@@ -20,31 +16,13 @@ import (
 )
 
 var (
-	awsRegion    string
-	started      time.Time
-	sqsClient    *sqs.SQS
-	sqsQueueURL  string
-	redisAddress string
-	redisClient  *redis.Client
-	logger       *logrus.Entry
+	started     time.Time
+	sqsQueueURL string
 )
 
 func init() {
-	// Parse command line flags (-aws-region AWS_REGION -redis REDIS_ADDRESS -sqs SQS_QUEUE_URL)
-	flag.StringVar(&awsRegion, "aws-region", "", "AWS Region")
-	flag.StringVar(&redisAddress, "redis", "127.0.0.1:6379", "Redis Address")
-	flag.StringVar(&sqsQueueURL, "sqs", "", "SQS Queue Url")
-	flag.Parse()
-	// overwrite with environment variables if set AWS_REGION=… REDIS_ADDRESS=… SQS_QUEUE_URL=…
-	if os.Getenv("AWS_REGION") != "" {
-		awsRegion = os.Getenv("AWS_REGION")
-	}
-	if os.Getenv("REDIS_ADDRESS") != "" {
-		redisAddress = os.Getenv("REDIS_ADDRESS")
-	}
-	if os.Getenv("SQS_QUEUE_URL") != "" {
-		sqsQueueURL = os.Getenv("SQS_QUEUE_URL")
-	}
+	// parse environment variables
+	sqsQueueURL = os.Getenv("SQS_QUEUE_URL")
 }
 
 func main() {
@@ -53,34 +31,25 @@ func main() {
 
 	// Set up components
 	components.InitLogger("SqsProcessor")
-	logger = dhelpersCache.GetLogger()
 	err = components.InitSentry()
 	dhelpers.CheckErr(err)
 	err = components.InitDiscord()
 	dhelpers.CheckErr(err)
 	components.InitTranslator(nil)
-
-	// connect to aws
-	sess := session.Must(session.NewSession(&aws.Config{
-		Region: aws.String(awsRegion),
-	}))
-	sqsClient = sqs.New(sess)
-
-	// connect to redis
-	redisClient = redis.NewClient(&redis.Options{
-		Addr:     redisAddress,
-		Password: "",
-		DB:       0,
-	})
-	dhelpersCache.SetRedisClient(redisClient)
+	components.InitRedis()
+	err = components.InitAwsSqs()
+	dhelpers.CheckErr(err)
 
 	// Setup all modules
 	modules.Init()
 
-	logger.Infoln("Processor booting completed, took", time.Since(started).String())
+	dhelpersCache.GetLogger().Infoln("Processor booting completed, took", time.Since(started).String())
 
 	// bot run loop
 	go func() {
+		sqsClient := dhelpersCache.GetAwsSqsSession()
+		logger := dhelpersCache.GetLogger()
+		redisClient := dhelpersCache.GetRedisClient()
 
 		for {
 			result, err := sqsClient.ReceiveMessage(&sqs.ReceiveMessageInput{
