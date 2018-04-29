be981e3be7c6a4f9d27be3f2ab27c6b31e180fa9
Sebastian Winkler <sebastian.winkler@slmn.de>
2018-04-29 17:08:58 +0200

    [gall] better gall icon

--- modules/gall/common.go
+++ modules/gall/common.go
@@ -13,7 +13,7 @@ var (
 	// GallColor is the discord colour used for Gall embeds
 	GallColor = dhelpers.GetDiscordColorFromHex("#4064D0")
 	// GallIcon is a Gall Logo
-	GallIcon = "https://i.imgur.com/SkVJwJP.jpg"
+	GallIcon = "https://i.imgur.com/tIYs6jt.png"
 
 	friendlyBoardURL = func(boardID string) string {
 		return "http://gall.dcinside.com/board/lists/?id=" + boardID + "&page=1&exception_mode=recommend"
