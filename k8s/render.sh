fa526754396842c62a9962eb9bb2ca0fa55a43b9
Sebastian Winkler <sebastian.winkler@slmn.de>
2019-02-16 13:30:06 +0100

    adds last.fm credentials deployment

--- k8s/render.sh
+++ k8s/render.sh
@@ -9,6 +9,8 @@
 # DOCKER_IMAGE_HASH
 # CONCURRENT_PROCESSING_LIMIT
 # DB_DSN
+# LASTFM_KEY
+# LASTFM_SECRET
 
 template="k8s/manifest.tmpl.yaml"
 target="k8s/manifest.yaml"
@@ -22,3 +24,5 @@ sed -i -e "s|{{DISCORD_TOKENS}}|$DISCORD_TOKENS|g" "$target"
 sed -i -e "s|{{DOCKER_IMAGE_HASH}}|$DOCKER_IMAGE_HASH|g" "$target"
 sed -i -e "s|{{CONCURRENT_PROCESSING_LIMIT}}|$CONCURRENT_PROCESSING_LIMIT|g" "$target"
 sed -i -e "s|{{DB_DSN}}|$DB_DSN|g" "$target"
+sed -i -e "s|{{LASTFM_KEY}}|$LASTFM_KEY|g" "$target"
+sed -i -e "s|{{LASTFM_SECRET}}|$LASTFM_SECRET|g" "$target"
