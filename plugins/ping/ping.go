fa4c68f151ad6db9a7baa744f1b4ecab68011f82
Sebastian Winkler <sebastian.winkler@slmn.de>
2019-05-11 21:36:34 +0200

    ping: more useful ping

--- plugins/ping/ping.go
+++ plugins/ping/ping.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/bwmarrin/discordgo"
+	"gitlab.com/Cacophony/go-kit/discord"
 
 	"gitlab.com/Cacophony/go-kit/events"
 )
@@ -15,28 +16,45 @@ func handlePing(event *events.Event) {
 		return
 	}
 
-	_, err = event.RespondComplex(
+	discordToGateway := event.ReceivedAt.Sub(createdAt).Round(time.Millisecond)
+	gatewayToProcessor := time.Since(event.ReceivedAt).Round(time.Millisecond)
+
+	sendStart := time.Now()
+	messages, err := event.RespondComplex(
 		&discordgo.MessageSend{
-			Embed: &discordgo.MessageEmbed{
-				Title: "ping.ping-response.embed.title",
-				Fields: []*discordgo.MessageEmbedField{
-					{
-						Name:   "ping.ping-response.embed.field.DiscordToGateway.title",
-						Value:  "ping.ping-response.embed.field.DiscordToGateway.value",
-						Inline: true,
-					},
-					{
-						Name:   "ping.ping-response.embed.field.GatewayToProcessor.title",
-						Value:  "ping.ping-response.embed.field.GatewayToProcessor.value",
-						Inline: true,
-					},
-				},
-			},
+			Embed: pingEmbed(),
 		},
 		"DiscordToGateway",
-		event.ReceivedAt.Sub(createdAt),
+		discordToGateway,
 		"GatewayToProcessor",
-		time.Since(event.ReceivedAt),
+		gatewayToProcessor,
+	)
+	if err != nil {
+		event.Except(err)
+		return
+	}
+	sendDuration := time.Since(sendStart).Round(time.Millisecond)
+
+	if len(messages) <= 0 {
+		return
+	}
+
+	_, err = discord.EditComplexWithVars(
+		nil,
+		event.Discord(),
+		event.Localizations(),
+		&discordgo.MessageEdit{
+			Embed:   pingEmbed(),
+			ID:      messages[0].ID,
+			Channel: messages[0].ChannelID,
+		},
+		false,
+		"DiscordToGateway",
+		discordToGateway,
+		"GatewayToProcessor",
+		gatewayToProcessor,
+		"SendDuration",
+		sendDuration,
 	)
 	event.Except(err)
 }
@@ -52,5 +70,28 @@ func handlePang(event *events.Event) {
 	_, err := event.Respond("ping.pang-response")
 	if err != nil {
 		event.Except(err)
+	}
+}
+
+func pingEmbed() *discordgo.MessageEmbed {
+	return &discordgo.MessageEmbed{
+		Title: "ping.ping-response.embed.title",
+		Fields: []*discordgo.MessageEmbedField{
+			{
+				Name:   "ping.ping-response.embed.field.DiscordToGateway.title",
+				Value:  "ping.ping-response.embed.field.DiscordToGateway.value",
+				Inline: false,
+			},
+			{
+				Name:   "ping.ping-response.embed.field.GatewayToProcessor.title",
+				Value:  "ping.ping-response.embed.field.GatewayToProcessor.value",
+				Inline: false,
+			},
+			{
+				Name:   "ping.ping-response.embed.field.SendDuration.title",
+				Value:  "ping.ping-response.embed.field.SendDuration.value",
+				Inline: false,
+			},
+		},
 	}
 }
