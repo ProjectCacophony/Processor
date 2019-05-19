4a08c3f9ea4850a658d37678bb89272449bbf603
Snakeyez <JohnBar216@gmail.com>
2019-05-19 14:08:54 -0500

    customcommands: prioritizes server commands first
    Also if multiple commands exist with the same name, one will be chosen at random

--- plugins/customcommands/command.go
+++ plugins/customcommands/command.go
@@ -1,6 +1,8 @@
 package customcommands
 
 import (
+	"math/rand"
+
 	"gitlab.com/Cacophony/go-kit/events"
 	"go.uber.org/zap"
 )
@@ -10,24 +12,48 @@ func (p *Plugin) ranCustomCommand(event *events.Event) bool {
 		return false
 	}
 
-	event.Logger().Info(event.Fields()[0])
-
-	var entires []Entry
-	err := p.db.Find(&entires, "name = ? and ((is_user_command = true and user_id = ?) or (is_user_command = false and guild_id = ?))",
+	// query entries
+	var entries []Entry
+	err := p.db.Find(&entries, "name = ? and ((is_user_command = true and user_id = ?) or (is_user_command = false and guild_id = ?))",
 		event.Fields()[0],
 		event.UserID,
 		event.GuildID,
-	).Order("is_user_command").Error
+	).Error
 	if err != nil {
 		event.Logger().Error("error querying custom commands", zap.Error(err))
 		return false
 	}
 
-	if len(entires) == 0 {
+	if len(entries) == 0 {
 		return false
 	}
 
-	event.Respond(entires[0].Content)
+	if len(entries) == 1 {
+		_, err = event.Respond(entries[0].Content)
+		event.Except(err)
+		return true
+	}
 
-	return true
+	var serverEntries []Entry
+	var userEntries []Entry
+	for _, entry := range entries {
+		if entry.IsUserCommand {
+			userEntries = append(userEntries, entry)
+		}
+		serverEntries = append(serverEntries, entry)
+	}
+
+	if len(serverEntries) > 0 {
+		seed := rand.Intn(len(serverEntries))
+		event.Respond(serverEntries[seed].Content)
+		return true
+	}
+
+	if len(userEntries) > 0 {
+		seed := rand.Intn(len(userEntries))
+		event.Respond(userEntries[seed].Content)
+		return true
+	}
+
+	return false
 }
