9cf38d1a029738c6a1c281306770a74bb3013f37
Sebastian Winkler <sebastian.winkler@slmn.de>
2019-06-01 17:44:13 +0200

    implements go-kit update, patreon: uses patreon permission for status

--- plugins/patreon/status.go
+++ plugins/patreon/status.go
@@ -1,25 +1,19 @@
 package patreon
 
 import (
-	"strings"
-
 	"gitlab.com/Cacophony/go-kit/events"
+	"gitlab.com/Cacophony/go-kit/permissions"
 )
 
 func (p *Plugin) handleStatus(event *events.Event) {
-	// TOOD: use patreon permission to check qualification instead
-
-	patron, err := getPatron(p.db, event.UserID)
-	if err != nil && !strings.Contains(err.Error(), "record not found") {
-		event.Except(err)
-		return
-	}
-
 	var activePatron bool
-	if patron != nil && patron.PatronStatus == "active_patron" {
+	if event.Has(permissions.Patron) {
 		activePatron = true
 	}
 
-	_, err = event.Respond("patreon.status.response", "activePatron", activePatron)
+	_, err := event.Respond(
+		"patreon.status.response",
+		"activePatron", activePatron,
+	)
 	event.Except(err)
 }
