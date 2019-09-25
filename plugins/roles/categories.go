31365def16a6e5769945b972e415f50351e7c26f
Snakeyez <JohnBar216@gmail.com>
2019-09-24 21:33:47 -0500

    impliments category editing

--- plugins/roles/categories.go
+++ plugins/roles/categories.go
@@ -22,7 +22,7 @@ func (p *Plugin) createCategory(event *events.Event) {
 		return
 	}
 
-	existingCategory, err := p.getCategoryByName(name)
+	existingCategory, err := p.getCategoryByName(name, event.GuildID)
 	if err != nil {
 		event.Except(err)
 		return
@@ -73,9 +73,72 @@ func (p *Plugin) createCategory(event *events.Event) {
 	)
 }
 
-func (p *Plugin) getCategoryByName(name string) (*Category, error) {
+func (p *Plugin) updateCategory(event *events.Event) {
+	if len(event.Fields()) < 7 {
+		event.Respond("common.invalid-params")
+		return
+	}
+
+	currentName := event.Fields()[3]
+	existingCategory, err := p.getCategoryByName(currentName, event.GuildID)
+	if err != nil {
+		event.Except(err)
+		return
+	}
+	if existingCategory.Name == "" {
+		event.Respond("roles.category.does-not-exist")
+		return
+	}
+
+	name := event.Fields()[4]
+	message := event.Fields()[5]
+	inputChannel := event.Fields()[6]
+
+	if name == "" {
+		event.Respond("roles.category.no-name")
+		return
+	}
+
+	channel, err := event.State().ChannelFromMention(event.GuildID, inputChannel)
+	if err != nil {
+		event.Except(err)
+		return
+	}
+
+	limit := 0
+	if len(event.Fields()) >= 8 {
+		limit, err = strconv.Atoi(event.Fields()[7])
+		if err != nil {
+			event.Respond("roles.category.limit-not-number")
+			return
+		}
+	}
+
+	pool := ""
+	if len(event.Fields()) >= 9 {
+		pool = event.Fields()[8]
+	}
+
+	existingCategory.ChannelID = channel.ID
+	existingCategory.Name = name
+	existingCategory.Message = message
+	existingCategory.Pool = pool
+	existingCategory.Limit = limit
+
+	err = p.db.Save(existingCategory).Error
+	if err != nil {
+		event.Except(err)
+		return
+	}
+
+	event.Respond("roles.category.update",
+		"category", existingCategory,
+	)
+}
+
+func (p *Plugin) getCategoryByName(name string, guildId string) (*Category, error) {
 	var category Category
-	err := p.db.First(&category, "name = ?", name).Error
+	err := p.db.First(&category, "name = ? and guild_id = ?", name, guildId).Error
 	if err != nil && !strings.Contains(err.Error(), "record not found") {
 		return nil, err
 	}
