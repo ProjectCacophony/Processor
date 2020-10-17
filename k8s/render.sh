0b1cb4b5d07cd205d6e1005dca76f4502e3c8e69
Sebastian Winkler <sebastian@winkler.dev>
2020-10-17 18:32:55 +0200

    adds new secret HONEYCOMB_API_KEY

--- k8s/render.sh
+++ k8s/render.sh
@@ -30,6 +30,7 @@
 # TRELLO_TOKEN
 # GOOGLE_MAPS_KEY
 # DARK_SKY_KEY
+# HONEYCOMB_API_KEY
 # base64 encoded file content:
 # GOOGLE_APPLICATION_CREDENTIALS
 
@@ -66,4 +67,5 @@ sed -i -e "s|{{INSTAGRAM_SESSION_IDS}}|$INSTAGRAM_SESSION_IDS|g" "$target"
 sed -i -e "s|{{TRELLO_KEY}}|$TRELLO_KEY|g" "$target"
 sed -i -e "s|{{TRELLO_TOKEN}}|$TRELLO_TOKEN|g" "$target"
 sed -i -e "s|{{DARK_SKY_KEY}}|$DARK_SKY_KEY|g" "$target"
+sed -i -e "s|{{HONEYCOMB_API_KEY}}|$HONEYCOMB_API_KEY|g" "$target"
 sed -i -e "s|{{GOOGLE_MAPS_KEY}}|$GOOGLE_MAPS_KEY|g" "$target"
