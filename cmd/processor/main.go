1c321ad5683df200ce87a5641f7adb57705026e1
Sebastian Winkler <sebastian@winkler.dev>
2020-10-17 21:40:43 +0200

    sets sample ratio config

--- cmd/processor/main.go
+++ cmd/processor/main.go
@@ -85,7 +85,13 @@ func main() {
 
 		provider := sdktrace.NewTracerProvider(
 			sdktrace.WithConfig(sdktrace.Config{
-				DefaultSampler: sdktrace.AlwaysSample(),
+				DefaultSampler: sdktrace.ParentBased(
+					sdktrace.TraceIDRatioBased(0.01),
+					sdktrace.WithRemoteParentSampled(sdktrace.AlwaysSample()),
+					sdktrace.WithRemoteParentNotSampled(sdktrace.TraceIDRatioBased(0.01)),
+					sdktrace.WithLocalParentSampled(sdktrace.AlwaysSample()),
+					sdktrace.WithLocalParentNotSampled(sdktrace.TraceIDRatioBased(0.01)),
+				),
 			}),
 			sdktrace.WithSyncer(honeycombExporter),
 		)
