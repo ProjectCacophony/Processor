bd09dfd9c0ad083540a0c811d73fe4be0091390a
Sebastian Winkler <sebastian@winkler.dev>
2020-10-19 17:34:46 +0200

    vlive: adds regex to validate VLive Channel IDs

--- plugins/vlive/extract.go
+++ plugins/vlive/extract.go
@@ -4,12 +4,19 @@ import (
 	"regexp"
 )
 
-var vliveRegexp = regexp.MustCompile(".*vlive.(tv|com)/(channels/)?([^/]+).*")
+var (
+	vliveRegexp          = regexp.MustCompile(".*vlive.(tv|com)/(channels/)?([^/]+).*")
+	vliveChannelIDRegexp = regexp.MustCompile("^[A-Z0-9]+$")
+)
 
 func extractVLiveChannelID(input string) string {
 	parts := vliveRegexp.FindStringSubmatch(input)
 	if len(parts) < 4 {
 		return input
+	}
+
+	if !vliveChannelIDRegexp.MatchString(parts[3]) {
+		return ""
 	}
 
 	return parts[3]
